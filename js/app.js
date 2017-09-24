30e47b5b4481fe0428011e4a51aad57ef38fb71e
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-09-24 00:05:46 -0400

    Work on adding event listeners

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-$(document).foundation()
+//$(document).foundation();
+
+class OverDrive{
+  constructor() {
+    this.setUpEventListeners();
+  }
+
+  setUpEventListeners() {
+    const actionsForm = document.querySelector('form#actions');
+    const addUsersBtn = actionsForm.querySelector('.add-users');
+    //console.log(addUsersBtn);
+    addUsersBtn.addEventListener('click', (e) => this.handleAddUsers);
+    //actionForm.addEventListener('submit', (e) => this.handleSubmit(e));
+  }
+
+  handleAddUsers(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+}
+
+const overDrive = new OverDrive();
