eafbf0b91b76ad185aea0ce87c5701065ee4e911
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-10-04 01:00:21 -0400

    Add OverDrive UI on over Drive UI

--- js/inject.js
+++ js/inject.js
@@ -2,22 +2,9 @@ function prependChild(parent, child) {
     parent.insertBefore(child, parent.firstChild);
 }
 
-var wrapper = document.createElement("div");
-wrapper.innerHTML = '\
-  <div class="grid-container">\
-  <div class="grid-x grid-padding-x">\
-      <div class="large-12 cell">\
-          <img id="logo" height="64">\
-      </div>\
-  </div>\
-  </div>\
-';
-wrapper.innerHTML = '<iframe id="extFrame" class="over_drive_frame" width="100%"></iframe>'
 const mainPage = document.querySelector("#drive_main_page");
-//mainPage.appendChild(wrapper);
-prependChild(mainPage, wrapper);
-//var imgURL = chrome.extension.getURL("img/logo.png");
-//mainPage.querySelector("#logo").src = imgURL;
+var overDriveFrame = document.createElement("div");
+overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="512"></iframe>'
+prependChild(mainPage, overDriveFrame);
 var frameURL = chrome.extension.getURL("index.html");
-mainPage.querySelector("#extFrame").src = frameURL;
-console.log("inject");
+mainPage.querySelector("#over_drive_frame").src = frameURL;
