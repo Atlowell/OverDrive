eb225c8ca6afa66b2a3099867bd91915f2188fa2
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-10-04 10:55:27 -0400

    Make OverDrive UI larger

--- js/inject.js
+++ js/inject.js
@@ -4,7 +4,7 @@ function prependChild(parent, child) {
 
 const mainPage = document.querySelector("#drive_main_page");
 var overDriveFrame = document.createElement("div");
-overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="512"></iframe>'
+overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="1024"></iframe>'
 prependChild(mainPage, overDriveFrame);
 var frameURL = chrome.extension.getURL("index.html");
 mainPage.querySelector("#over_drive_frame").src = frameURL;
