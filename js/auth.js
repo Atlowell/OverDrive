59b194e08b58763b86790ea10fa599244c4d9633
Atlowell <atlowell42@gmail.com>
2017-10-06 00:20:11 -0400

    Changed authorization to use chrome.identity.  It seems to work, and it gets a token, however trying to call a drive function fails with invaid credentials.

--- js/auth.js
+++ js/auth.js
@@ -31,5 +31,38 @@ function clientInitAuth() {
     });
 }
 
-clientLoadAuth();
-setupOverDrive();
+function identityAuth() {
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        if(chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError);
+        }
+        else {
+            console.log("Token: " + token);
+            
+            // Test code, not my own
+            /*var x = new XMLHttpRequest();
+            x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+            x.onload = function() {
+                console.log(x.response);
+            };
+            x.send(); */
+            
+            
+            return token;
+        }
+    });
+}
+
+tok = identityAuth();
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "https://www.googleapis.com/drive/v2/files/" + "1GDoJXHzDnPKo4IV1PBZDViLv3dDLCqf1IYN9yTYGhAk");
+xhr.setRequestHeader('Authorization', 'Bearer ' + tok);
+xhr.onload = function() {
+    console.log(xhr.response);
+};
+xhr.onerror = function() {
+    console.log(xhr.response);
+}
+xhr.send();
+//clientLoadAuth();
+//setupOverDrive();
