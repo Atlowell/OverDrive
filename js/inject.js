4c2f3004b9a3e04f36d155b94c544f872e468ed9
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-10-15 14:59:22 -0400

    Toggle UI and change icons

--- js/inject.js
+++ js/inject.js
@@ -2,9 +2,13 @@ function prependChild(parent, child) {
     parent.insertBefore(child, parent.firstChild);
 }
 
-const mainPage = document.querySelector("#drive_main_page");
-var overDriveFrame = document.createElement("div");
-overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="1024"></iframe>'
-prependChild(mainPage, overDriveFrame);
-var frameURL = chrome.extension.getURL("index.html");
-mainPage.querySelector("#over_drive_frame").src = frameURL;
+if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame") == null) {
+    var overDriveFrame = document.createElement("div");
+    overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="1024"></iframe>'
+    prependChild(document.querySelector("#drive_main_page"), overDriveFrame);
+    document.querySelector("#drive_main_page").querySelector("#over_drive_frame").src = chrome.extension.getURL("index.html");
+} else if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height > 0) {
+    document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height = 0;
+} else if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height == 0) {
+    document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height = 1024;
+}
