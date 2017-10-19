c42ba8d94dcf99a1259faa736cae6c2107448353
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-10-19 15:35:48 -0400

    Make page refresh instead of hide UI when toggling

--- js/inject.js
+++ js/inject.js
@@ -7,8 +7,6 @@ if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame"
     overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="1024"></iframe>'
     prependChild(document.querySelector("#drive_main_page"), overDriveFrame);
     document.querySelector("#drive_main_page").querySelector("#over_drive_frame").src = chrome.extension.getURL("index.html");
-} else if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height > 0) {
-    document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height = 0;
-} else if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height == 0) {
-    document.querySelector("#drive_main_page").querySelector("#over_drive_frame").height = 1024;
+} else  {
+    window.location.reload(false); 
 }
