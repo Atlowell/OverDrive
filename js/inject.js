a1420d5b0fb79f4bad64abb23f1529254344f1c4
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-12-04 16:39:13 -0500

    Fix scrolling

--- js/inject.js
+++ js/inject.js
@@ -1,10 +1,10 @@
 function prependChild(parent, child) {
     parent.insertBefore(child, parent.firstChild);
 }
-
+var height = window.innerHeight;
 if (document.querySelector("#drive_main_page").querySelector("#over_drive_frame") == null) {
     var overDriveFrame = document.createElement("div");
-    overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="1024"></iframe>'
+    overDriveFrame.innerHTML = '<iframe id="over_drive_frame" class="over_drive_frame" width="100%" height="' + height + '" scrolling="yes"></iframe>'
     prependChild(document.querySelector("#drive_main_page"), overDriveFrame);
     document.querySelector("#drive_main_page").querySelector("#over_drive_frame").src = chrome.extension.getURL("index.html");
 } else  {
